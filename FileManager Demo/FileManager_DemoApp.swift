c566fc41db431fb62bdf589e4bf925fdfe5677c4
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2024-11-12 11:27:15 -0600

    Initial Commit

---
+++ FileManager Demo/FileManager_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManager_DemoApp.swift
+//  FileManager Demo
+//
+//  Created by Randy McKown on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FileManager_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
